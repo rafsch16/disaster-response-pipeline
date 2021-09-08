0eb6cdaf24336560e3325a14e89214ef7c92cde0
rafsch16 <rafael.schoch84@gmail.com>
2021-09-08 09:50:28 +0000

    train_classifier.py completed

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,147 @@
+# import libraries
 import sys
+import os
+import pandas as pd
+import re
+import numpy as np
+import pickle
+import nltk
+
+# download necessary NLTK data
+nltk.download(['punkt','wordnet'])
+
+# import statements SQL
+from sqlalchemy import create_engine
+
+# import NLTK statements
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+# import statements sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
 
 
 def load_data(database_filepath):
-    pass
-
+    '''
+    Loads the data from a SQL table.
+    
+    Parameters:
+        database_filepath (str): file path to the database
+    
+    Returns:
+        X (np.array): Explanatory variable
+        Y (np.array): Response variables
+        category_names (np.array): Response categories
+    '''
+    # obtain path to the database
+    path ="sqlite:////home/workspace/"+ database_filepath
+    
+    # create a database engine
+    engine = create_engine(path)
+    
+    # read the SQL table into DataFrame objects
+    df = pd.read_sql_table('disaster_response_data', engine)
+    X = df['message'].values
+    Y = df.iloc[:,4:].values
+    
+    # obtain category names
+    category_names = df.iloc[:,4:].columns.values
+    
+    return X, Y, category_names
 
 def tokenize(text):
-    pass
+    '''
+    Tokenize a text into words and reduce words to their root forms.
+    
+    Parameters:
+        text (str)
+    
+    Returns:
+        clean_tokens (list)
+    '''
+    # split text into words
+    tokens = word_tokenize(text)
+    
+    # initialize lemmatizer object
+    lemmatizer = WordNetLemmatizer()
+    
+    # lemmatize and clean words
+    clean_tokens=[]
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+        
+    return clean_tokens
 
 
 def build_model():
-    pass
+    '''
+    Builds a multi-output model for text calssification using the sklearn 
+    Pipline class.
+    
+    The pipline includes:
+    Transforms - CountVectorizer, TfidfTransformer
+    Estimator - RandomForestClassifier
+    
+    GridSearchCV is used to find the best parameters for the model
+    
+    Returns:
+        model (estimator object)
+    '''
+    # create Pipline
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf',TfidfTransformer()),
+        ('clf',MultiOutputClassifier(RandomForestClassifier()))
+    ])
+    
+    # specify parameter values for the grid search
+    parameters = {
+        'vect__max_features': (1000, 2000, 5000),
+        'vect__max_df': (0.5, 0.75, 1),
+        'clf__estimator__min_samples_split': [10, 20, 30],
+    }
+    
+    # create model
+    model = GridSearchCV(pipeline, param_grid=parameters, n_jobs=-1)
 
+    return model
+    
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    '''
+    Evaluates the f1 score, precision and recall for each category.
+    
+    Parameters:
+        model (estimator object)
+        X_test (np.array): Explanatory variable 
+        Y_test (np.array): Response variables
+        category_names (np.array): Category names
+    '''
+    # predict on test data
+    Y_pred = model.predict(X_test)
+    
+    # print the scores for each category
+    for i in range(0,Y_test.shape[1],1):
+        print(category_names[i])
+        print(classification_report(Y_test[:,i], Y_pred[:,i]))
 
 
 def save_model(model, model_filepath):
-    pass
+    '''
+    Saves the model.
+    
+    Parameters:
+        model (estimator object)
+        model_filepath (str): File path to save the model
+    '''
+    pickle.dump(model, open(model_filepath, 'wb'))
 
 
 def main():
